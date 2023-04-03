58f2ce168caa9ca42100dd0131ee093d80d7ed63
AslimTaslam <aslimtaslam51@gmail.com>
2023-04-03 16:32:30 +0300

    Add helmet and express-rate-limit

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import { config } from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
 import userRoute from './routes/user-routes.js';
 
 config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(limiter);
 
 // routes
 app.use('/api/user', userRoute);
